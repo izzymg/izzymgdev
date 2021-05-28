1d315290af9d8ed2c600c43709ccf46f6dba193f
izzymg <izzymhg@gmail.com>
2021-05-29 10:33:56 +1200

    Add src/dist folders, build script to prefix css

---
+++ index.js
@@ -0,0 +1,27 @@
+const postcss = require("postcss");
+const autoprefixer = require("autoprefixer");
+
+const fs = require("fs").promises;
+
+const inputPath = "./src/css/index.css";
+const outputPath = "./dist/css/index.css";
+
+const readCss = async() => {
+  return fs.readFile(inputPath);
+};
+
+const process = buffer => {
+  return new Promise((resolve) => {
+    postcss([autoprefixer]).process(buffer).then(v => {
+      resolve(v.css);
+    });
+  });
+};
+
+const writeCss = str => {
+  return fs.writeFile(outputPath, str);
+};
+
+readCss().then(process).then(writeCss).then(() => {
+  console.log("Build done!");
+});
