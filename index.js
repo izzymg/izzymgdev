848eb844461ed1369a913c7f4f405ecf571e93ca
izzymg <izzymhg@gmail.com>
2021-05-31 13:07:49 +1200

    Build now creates & copies entire src to ./dist

--- index.js
+++ index.js
@@ -1,27 +1,73 @@
 const postcss = require("postcss");
 const autoprefixer = require("autoprefixer");
-
 const fs = require("fs").promises;
+const copyDir = require("./copy-dir");
 
-const inputPath = "./src/css/index.css";
-const outputPath = "./dist/css/index.css";
+const cssInputPath = "./src/css/index.css";
+const cssOutputPath = "./dist/css/index.css";
 
-const readCss = async() => {
-  return fs.readFile(inputPath);
+// Ignore dist not already existing when clearing it
+const clearDist = async() => {
+  try {
+    await fs.rm("./dist", { recursive: true })
+  } catch(e) {
+    if(e.code !== "ENOENT") {
+      throw e;
+    }
+  }
 };
 
-const process = buffer => {
+const makeDist = () => fs.mkdir("./dist/css", { recursive: true });
+
+const readCss = () => fs.readFile(cssInputPath, "utf8");
+const writeCss = () => fs.writeFile(cssOutputPath, "utf8");
+const copyHtml = () => fs.copyFile("./src/index.html", "./dist/index.html");
+const copyAssets = () => copyDir("./src/assets", "./dist", {  });
+
+const processCss = buffer => {
   return new Promise((resolve) => {
-    postcss([autoprefixer]).process(buffer, { from: inputPath, to: outputPath }).then(v => {
+    postcss([autoprefixer]).process(buffer, { from: cssInputPath, to: cssOutputPath }).then(v => {
       resolve(v.css);
     });
   });
 };
 
-const writeCss = str => {
-  return fs.writeFile(outputPath, str);
+const buildAll = () => {
+  let buildCss = readCss().
+    then(processCss)
+    .then(writeCss)
+    .then(() => console.log("css built successfully.."));
+
+  let copyRest = copyHtml()
+    .then(() => console.log("html built successfully.."))
+    .then(() => copyAssets()
+    .then(() => console.log("assets built successfully..")));
+
+  Promise.all([buildCss, copyRest])
+    .then(() => {
+      console.log("Build finished");
+    }).catch(e => {
+      console.error("Build errored:", e);
+    });
 };
 
-readCss().then(process).then(writeCss).then(() => {
-  console.log("Build done!");
-});
+const main = async() => {
+
+  try {
+    await clearDist();
+    // await new Promise(resolve => setTimeout(resolve, 1500));
+    await makeDist();
+  } catch(e) {
+    console.error("failed to regenerate ./dist", e);
+  } finally {
+    console.log("./dist generated");
+  }
+
+  try {
+    await buildAll();
+  } catch(e) {
+    console.error("build errored: ", e);
+  }
+};
+
+main();
